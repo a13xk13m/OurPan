4b1181bc182bd40c813e7e4a10afadc5d529408a
Alex Klementovich <a13k13m@gmail.com>
2022-01-13 11:15:54 -0500

    Home feed data interface.

--- lib/utils/home_feed_dao.dart
+++ lib/utils/home_feed_dao.dart
@@ -1,3 +1,33 @@
 // TODO: Build home feed interface to generate a feed for the user.
+// DAO handling getting pages.
+import 'package:firebase_database/firebase_database.dart';
+import 'package:our_pan/utils/recipe_dao.dart';
 
-class HomeFeedDao {}
+class HomeFeedDao {
+  // Reference to all recipes
+  final DatabaseReference ref = FirebaseDatabase.instance.ref('/recipes');
+
+  // Gets the first 100 recipes and returns a list of RecipeModels.
+  // TODO: implement timestamp based pagination.
+  Future<List<RecipeModel>> getRecipeFeed({int length = 100}) async {
+    List<RecipeModel> recipes = <RecipeModel>[];
+    DatabaseEvent feed = await ref.once();
+    DataSnapshot feedData = feed.snapshot;
+    // Determine length of feed, if the # of children of snapshot is less than
+    // the passed length, use that.
+    int lengthFeed =
+        length > feedData.children.length ? length : feedData.children.length;
+    // Iterate through the first 100 children.
+    for (int i = 0; i < lengthFeed; ++i) {
+      DataSnapshot item = feedData.children.elementAt(i);
+      Map<dynamic, dynamic> json = {};
+      for (var i in item.children) {
+        json[i.key] = i.value;
+      }
+      // Add recipeID to json.
+      json['recipeID'] = item.key;
+      recipes.add(RecipeModel.fromJson(json));
+    }
+    return recipes;
+  }
+}
